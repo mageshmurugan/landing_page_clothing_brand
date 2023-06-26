598408d400698da96933a672a0fd406f9b0fb2e0
mageshmurugan <mageshmurugan68@gmail.com>
2023-06-26 19:21:25 +0530

    fourth commit

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head>
+        <meta  name="viewport" content=
+            "width=device-width, user-scalable=no" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
